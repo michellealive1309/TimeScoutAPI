5956f33505dc6dc54f60b6acfe326120dd7d62cb
Pitiphat <michellealive1309@gmail.com>
2025-01-09 21:06:48 +0700

    Add role on User

--- TimeScout.API/Models/User.cs
+++ TimeScout.API/Models/User.cs
@@ -8,6 +8,7 @@ public class User
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
+    public required string Role { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateTime CreatedAt { get; set; }
