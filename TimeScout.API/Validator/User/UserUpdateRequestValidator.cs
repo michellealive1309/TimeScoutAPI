11cb45ac259cb4519de9bd8578380d667a33faa9
Pitiphat <michellealive1309@gmail.com>
2025-01-10 17:42:15 +0700

    Add UserUpdateRequestValidator for validating user update requests

---
+++ TimeScout.API/Validator/User/UserUpdateRequestValidator.cs
@@ -0,0 +1,24 @@
+using System;
+using FluentValidation;
+using TimeScout.API.DTOs.User;
+
+namespace TimeScout.API.Validator.User;
+
+public class UserUpdateRequestValidator : AbstractValidator<UserUpdateRequestDto>
+{
+    public UserUpdateRequestValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required")
+            .MinimumLength(6).WithMessage("Username must be at least 6 characters");
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MinimumLength(1).WithMessage("First name must be at least 1 character")
+            .MaximumLength(100).WithMessage("First name must not over than 100 characters");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MinimumLength(1).WithMessage("Last name must be at least 1 character")
+            .MaximumLength(100).WithMessage("Last name must not over than 100 characters");
+    }
+}
