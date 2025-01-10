390921a7caab958d68e9503aab1c46a0ac9e34c2
Pitiphat <michellealive1309@gmail.com>
2025-01-10 15:23:59 +0700

    Add logic to manage created and updated date on db context SaveChangesAsync

--- TimeScout.API/DataAccess/TimeScoutDbContext.cs
+++ TimeScout.API/DataAccess/TimeScoutDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using TimeScout.API.Models;
 using Microsoft.EntityFrameworkCore;
 using TimeScout.API.DataAccess.ModelConfigurations;
+using TimeScout.API.Entity;
 
 namespace TimeScout.API.DataAccess;
 
@@ -26,5 +27,25 @@ public class TimeScoutDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSnakeCaseNamingConvention();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is BaseEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entry in entries)
+        {
+            var entity = (BaseEntity)entry.Entity;
+            if (entry.State == EntityState.Added)
+            {
+                entity.Created = DateTime.UtcNow;
+            }
+
+            entity.Modified = DateTime.UtcNow;
+        }
+
+        ChangeTracker.DetectChanges();
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
