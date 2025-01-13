db37f5c09e5b61a4e1a5ea63961eed9d4f7b5d7a
Pitiphat <michellealive1309@gmail.com>
2025-01-13 17:36:13 +0700

    Add RecoveryEndDate property to User model

--- TimeScout.API/Models/User.cs
+++ TimeScout.API/Models/User.cs
@@ -13,5 +13,6 @@ public class User : BaseEntity
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? RefreshToken { get; set; }
+    public DateTime? RecoveryEndDate { get; set; }
     public ICollection<EventGroup>? EventGroups { get; set; }
 }
