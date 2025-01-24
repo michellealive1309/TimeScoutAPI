b346cf64f59b2e4a20d1fd22f66646103da5032f
Pitiphat <michellealive1309@gmail.com>
2025-01-24 15:01:25 +0700

    Rename DbSet property for EventGroup to EventGroups for clarity

--- TimeScout.API/DataAccess/TimeScoutDbContext.cs
+++ TimeScout.API/DataAccess/TimeScoutDbContext.cs
@@ -13,7 +13,7 @@ public class TimeScoutDbContext : DbContext
     }
 
     public DbSet<Event> Events { get; set; }
-    public DbSet<EventGroup> Groups { get; set; }
+    public DbSet<EventGroup> EventGroups { get; set; }
     public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
