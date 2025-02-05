17d8cce4b25426e128382b28817e7cbd7955ad3a
Pitiphat <michellealive1309@gmail.com>
2025-02-05 16:14:04 +0700

    Add UserId property to Tag model and inherit from BaseEntity

--- TimeScout.API/Models/Tag.cs
+++ TimeScout.API/Models/Tag.cs
@@ -1,8 +1,11 @@
+using TimeScout.API.Entity;
+
 namespace TimeScout.API.Models;
 
-public class Tag
+public class Tag : BaseEntity
 {
     public int Id { get; set; }
     public required string Name { get; set; }
     public string? Color { get; set; }
+    public int UserId { get; set; }
 }
